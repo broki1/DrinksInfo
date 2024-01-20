0b51c2b2d1c0ef172d3416ce37584feb9130a3b6
Justice Engle <justice.engle@icloud.com>
2024-01-19 20:09:50 -0500

    Add project files.

---
+++ DrinksInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Net.Http;
+
+namespace DrinksInfo;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+
+        client.DefaultRequestHeaders.Accept.Clear();
+
+        await GetDrinkCategories(client);
+    }
+
+    static async Task GetDrinkCategories(HttpClient client)
+    {
+        var drinkCategoriesAPI = ConfigurationManager.AppSettings.Get("DrinkCategories");
+
+        var drinkCategories = await client.GetStringAsync(drinkCategoriesAPI);
+
+        Console.WriteLine(drinkCategories);
+    }
+}
